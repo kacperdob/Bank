c81bad0fb23ea159d3930c9f12ec267df4542020
Kacper Dobrowolski <kacper.dob@gmail.com>
2019-02-02 14:50:09 +0100

    limit konta

--- src/com/dobr/AccBonus.java
+++ src/com/dobr/AccBonus.java
@@ -3,11 +3,8 @@ package com.dobr;
 public class AccBonus {
     int money;
 
-    void getBunus
+    void getBunus()
     {
         money += 100;
-        int money2;
-        money2 = money + 50;
-
     }
 }
